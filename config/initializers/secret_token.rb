50c47803dc1043960fa739dcbfece8d6ed982e7b
Marcos Hack <marcoshack@gmail.com>
2012-07-06 11:05:19 -0300

    criacao do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afeed::Application.config.secret_token = '12ea7df574579b9b0cc050119a73468f355671e751ad026805665f2b2aad8d454276adb020184d679b035c42d6f11906f0790b5e1ec57e4c0ae146c99049ec79'
